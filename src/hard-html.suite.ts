a19c44ce6ecd92e09bfd084bd255a25c8a2e112d
Adrien Belminksy <belminksy@gmail.com>
2022-01-21 02:16:29 +0100

    feat: add hard suite

---
+++ src/hard-html.suite.ts
@@ -0,0 +1,147 @@
+/**
+ * Copyright (c) Minimouli
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { Executable, Path } from '@minimouli/framework'
+import Observer from './objects/Observer'
+
+suite('Hard Threshold', () => {
+
+    suite('HTML', () => {
+
+        test('Amazon', async () => {
+
+            const file = Path.fromMouli('res/html/amazon.html')
+
+            const exec1 = new Executable('antman', [
+                file, '2'
+            ])
+            await exec1.execute()
+
+            expect(exec1).toExitWith(0)
+
+            const minified = Path.fromAbsolute(exec1.serialize().stdio.stdout)
+
+            const exec2 = new Executable('giantman', [
+                minified, '2'
+            ])
+            await exec2.execute()
+
+            await expect(exec2).concurrent.toOutput(file)
+            expect(exec2).toExitWith(0)
+
+            const observer = new Observer(file, minified)
+
+            observer.checkScoreIsAbove(25)
+        })
+
+        test('Github', async () => {
+
+            const file = Path.fromMouli('res/html/github.html')
+
+            const exec1 = new Executable('antman', [
+                file, '2'
+            ])
+            await exec1.execute()
+
+            expect(exec1).toExitWith(0)
+
+            const minified = Path.fromAbsolute(exec1.serialize().stdio.stdout)
+
+            const exec2 = new Executable('giantman', [
+                minified, '2'
+            ])
+            await exec2.execute()
+
+            await expect(exec2).concurrent.toOutput(file)
+            expect(exec2).toExitWith(0)
+
+            const observer = new Observer(file, minified)
+
+            observer.checkScoreIsAbove(25)
+        })
+
+        test('Google', async () => {
+
+            const file = Path.fromMouli('res/html/google.html')
+
+            const exec1 = new Executable('antman', [
+                file, '2'
+            ])
+            await exec1.execute()
+
+            expect(exec1).toExitWith(0)
+
+            const minified = Path.fromAbsolute(exec1.serialize().stdio.stdout)
+
+            const exec2 = new Executable('giantman', [
+                minified, '2'
+            ])
+            await exec2.execute()
+
+            await expect(exec2).concurrent.toOutput(file)
+            expect(exec2).toExitWith(0)
+
+            const observer = new Observer(file, minified)
+
+            observer.checkScoreIsAbove(25)
+        })
+
+        test('Hacker News', async () => {
+
+            const file = Path.fromMouli('res/html/hackernews.html')
+
+            const exec1 = new Executable('antman', [
+                file, '2'
+            ])
+            await exec1.execute()
+
+            expect(exec1).toExitWith(0)
+
+            const minified = Path.fromAbsolute(exec1.serialize().stdio.stdout)
+
+            const exec2 = new Executable('giantman', [
+                minified, '2'
+            ])
+            await exec2.execute()
+
+            await expect(exec2).concurrent.toOutput(file)
+            expect(exec2).toExitWith(0)
+
+            const observer = new Observer(file, minified)
+
+            observer.checkScoreIsAbove(25)
+        })
+
+        test('Wikipedia', async () => {
+
+            const file = Path.fromMouli('res/html/wikipedia.html')
+
+            const exec1 = new Executable('antman', [
+                file, '2'
+            ])
+            await exec1.execute()
+
+            expect(exec1).toExitWith(0)
+
+            const minified = Path.fromAbsolute(exec1.serialize().stdio.stdout)
+
+            const exec2 = new Executable('giantman', [
+                minified, '2'
+            ])
+            await exec2.execute()
+
+            await expect(exec2).concurrent.toOutput(file)
+            expect(exec2).toExitWith(0)
+
+            const observer = new Observer(file, minified)
+
+            observer.checkScoreIsAbove(25)
+        })
+
+    })
+
+})
---
+++ src/hard-songs.suite.ts
@@ -0,0 +1,277 @@
+/**
+ * Copyright (c) Minimouli
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { Executable, Path } from '@minimouli/framework'
+import Observer from './objects/Observer'
+
+suite('Hard Threshold', () => {
+
+    suite('Songs', () => {
+
+        test('Meat Loaf - If I Can\'t Have You', async () => {
+
+            const file = Path.fromMouli('res/songs/if-i-cant-have-you.lyr')
+
+            const exec1 = new Executable('antman', [
+                file, '1'
+            ])
+            await exec1.execute()
+
+            expect(exec1).toExitWith(0)
+
+            const minified = Path.fromAbsolute(exec1.serialize().stdio.stdout)
+
+            const exec2 = new Executable('giantman', [
+                minified, '1'
+            ])
+            await exec2.execute()
+
+            await expect(exec2).concurrent.toOutput(file)
+            expect(exec2).toExitWith(0)
+
+            const observer = new Observer(file, minified)
+
+            observer.checkScoreIsAbove(25)
+        })
+
+        test('Rick Astley - Never Gonna Give You Up', async () => {
+
+            const file = Path.fromMouli('res/songs/never-gonna-give-you-up.lyr')
+
+            const exec1 = new Executable('antman', [
+                file, '1'
+            ])
+            await exec1.execute()
+
+            expect(exec1).toExitWith(0)
+
+            const minified = Path.fromAbsolute(exec1.serialize().stdio.stdout)
+
+            const exec2 = new Executable('giantman', [
+                minified, '1'
+            ])
+            await exec2.execute()
+
+            await expect(exec2).concurrent.toOutput(file)
+            expect(exec2).toExitWith(0)
+
+            const observer = new Observer(file, minified)
+
+            observer.checkScoreIsAbove(25)
+        })
+
+        test('Eminem - Rap God', async () => {
+
+            const file = Path.fromMouli('res/songs/rap-god.lyr')
+
+            const exec1 = new Executable('antman', [
+                file, '1'
+            ])
+            await exec1.execute()
+
+            expect(exec1).toExitWith(0)
+
+            const minified = Path.fromAbsolute(exec1.serialize().stdio.stdout)
+
+            const exec2 = new Executable('giantman', [
+                minified, '1'
+            ])
+            await exec2.execute()
+
+            await expect(exec2).concurrent.toOutput(file)
+            expect(exec2).toExitWith(0)
+
+            const observer = new Observer(file, minified)
+
+            observer.checkScoreIsAbove(25)
+        })
+
+        test('Rihanna - Work', async () => {
+
+            const file = Path.fromMouli('res/songs/work.lyr')
+
+            const exec1 = new Executable('antman', [
+                file, '1'
+            ])
+            await exec1.execute()
+
+            expect(exec1).toExitWith(0)
+
+            const minified = Path.fromAbsolute(exec1.serialize().stdio.stdout)
+
+            const exec2 = new Executable('giantman', [
+                minified, '1'
+            ])
+            await exec2.execute()
+
+            await expect(exec2).concurrent.toOutput(file)
+            expect(exec2).toExitWith(0)
+
+            const observer = new Observer(file, minified)
+
+            observer.checkScoreIsAbove(25)
+        })
+
+        test('Harry Belafonte - Jump in the Line', async () => {
+
+            const file = Path.fromMouli('res/songs/jump-in-the-line.lyr')
+
+            const exec1 = new Executable('antman', [
+                file, '1'
+            ])
+            await exec1.execute()
+
+            expect(exec1).toExitWith(0)
+
+            const minified = Path.fromAbsolute(exec1.serialize().stdio.stdout)
+
+            const exec2 = new Executable('giantman', [
+                minified, '1'
+            ])
+            await exec2.execute()
+
+            await expect(exec2).concurrent.toOutput(file)
+            expect(exec2).toExitWith(0)
+
+            const observer = new Observer(file, minified)
+
+            observer.checkScoreIsAbove(25)
+        })
+
+        test('Bug Hunter - Disco! in the Panic Room', async () => {
+
+            const file = Path.fromMouli('res/songs/disco-in-the-panic-room.lyr')
+
+            const exec1 = new Executable('antman', [
+                file, '1'
+            ])
+            await exec1.execute()
+
+            expect(exec1).toExitWith(0)
+
+            const minified = Path.fromAbsolute(exec1.serialize().stdio.stdout)
+
+            const exec2 = new Executable('giantman', [
+                minified, '1'
+            ])
+            await exec2.execute()
+
+            await expect(exec2).concurrent.toOutput(file)
+            expect(exec2).toExitWith(0)
+
+            const observer = new Observer(file, minified)
+
+            observer.checkScoreIsAbove(11)
+        })
+
+        test('Baio - Sister of Pearl', async () => {
+
+            const file = Path.fromMouli('res/songs/sister-of-pearl.lyr')
+
+            const exec1 = new Executable('antman', [
+                file, '1'
+            ])
+            await exec1.execute()
+
+            expect(exec1).toExitWith(0)
+
+            const minified = Path.fromAbsolute(exec1.serialize().stdio.stdout)
+
+            const exec2 = new Executable('giantman', [
+                minified, '1'
+            ])
+            await exec2.execute()
+
+            await expect(exec2).concurrent.toOutput(file)
+            expect(exec2).toExitWith(0)
+
+            const observer = new Observer(file, minified)
+
+            observer.checkScoreIsAbove(25)
+        })
+
+        test('Shirley Ellis - The New Game', async () => {
+
+            const file = Path.fromMouli('res/songs/the-new-game.lyr')
+
+            const exec1 = new Executable('antman', [
+                file, '1'
+            ])
+            await exec1.execute()
+
+            expect(exec1).toExitWith(0)
+
+            const minified = Path.fromAbsolute(exec1.serialize().stdio.stdout)
+
+            const exec2 = new Executable('giantman', [
+                minified, '1'
+            ])
+            await exec2.execute()
+
+            await expect(exec2).concurrent.toOutput(file)
+            expect(exec2).toExitWith(0)
+
+            const observer = new Observer(file, minified)
+
+            observer.checkScoreIsAbove(25)
+        })
+
+        test('Lyre Le Temps - Rob the Banker', async () => {
+
+            const file = Path.fromMouli('res/songs/rob-the-banker.lyr')
+
+            const exec1 = new Executable('antman', [
+                file, '1'
+            ])
+            await exec1.execute()
+
+            expect(exec1).toExitWith(0)
+
+            const minified = Path.fromAbsolute(exec1.serialize().stdio.stdout)
+
+            const exec2 = new Executable('giantman', [
+                minified, '1'
+            ])
+            await exec2.execute()
+
+            await expect(exec2).concurrent.toOutput(file)
+            expect(exec2).toExitWith(0)
+
+            const observer = new Observer(file, minified)
+
+            observer.checkScoreIsAbove(25)
+        })
+
+        test('System of a Down - Chop Suey!', async () => {
+
+            const file = Path.fromMouli('res/songs/chop-suey.lyr')
+
+            const exec1 = new Executable('antman', [
+                file, '1'
+            ])
+            await exec1.execute()
+
+            expect(exec1).toExitWith(0)
+
+            const minified = Path.fromAbsolute(exec1.serialize().stdio.stdout)
+
+            const exec2 = new Executable('giantman', [
+                minified, '1'
+            ])
+            await exec2.execute()
+
+            await expect(exec2).concurrent.toOutput(file)
+            expect(exec2).toExitWith(0)
+
+            const observer = new Observer(file, minified)
+
+            observer.checkScoreIsAbove(25)
+        })
+
+    })
+
+})
